20f810c838898ffd97b8267852dea5efbf19eb50
Anderson <eusouanderson@outlook.com>
2023-07-10 18:22:45 -0300

    "updte"

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def start():
     response = make_response('')
-    response.set_cookie('cookie_name', 'cookie_value', samesite='None', secure=True)
+    response.set_cookie('cookie_name', 'cookie_value', domain='eusouanderson.onrender.com')
     return render_template('index.html')
 
 @app.errorhandler(404)  
 def page_not_found(error):
-    return render_template('not_found.html'), 404
+    return render_template('index.html'), 404
 
 
 if __name__ == '__main__':
