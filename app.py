3607370552a41fd6dd9a80fd52ca98de9c01b2ce
Anderson <eusouanderson@outlook.com>
2023-07-18 15:56:37 -0300

    "update"

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, make_response
+from datetime import date
 
 debug=True
 
 app = Flask(__name__)
+date = f"{date.today()} "
+
 
 @app.route('/')
 def start():
     response = make_response('')
     response.set_cookie('cookie_name', 'cookie_value', domain='eusouanderson.onrender.com')
-    return render_template('index.html')
+    return render_template('index.html' , date = date)
 
 @app.errorhandler(404)  
 def page_not_found(error):
     return render_template('index.html'), 404
+
+print(date)
 
 
 if __name__ == '__main__':
